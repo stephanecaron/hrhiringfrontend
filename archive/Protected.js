d524eab69436016052812d740eb0de84447dc8d1
stephanecaron <stephane.caron@groupesavoie.com>
2023-02-02 10:10:45 -0400

    fixing pathing?

--- archive/Protected.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const Protected = ({ isLoggedIn, children }) => {
-  if (!isLoggedIn) {
-    return <Navigate to="/Login" replace />;
-  }
-  return children;
-};
-export default Protected;
